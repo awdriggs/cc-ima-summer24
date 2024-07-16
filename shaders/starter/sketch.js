a301a178aa69170ec40accd40ff354a0a2315e9e
Adam Driggers <awdriggs@gmail.com>
2024-07-15 23:24:04 -0400

    update

---
+++ shaders/starter/sketch.js
@@ -0,0 +1,33 @@
+// a shader variable
+let theShader;
+
+function preload(){
+  // load the shader
+  theShader = loadShader('shader.vert', 'shader.frag');
+}
+
+function setup() {
+  // disables scaling for retina screens which can create inconsistent scaling between displays
+  pixelDensity(1);
+  
+  // shaders require WEBGL mode to work
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+}
+
+function draw() {  
+  // shader() sets the active shader with our shader
+  shader(theShader);
+
+  //???
+  theShader.setUniform("u_resolution", [width, height]);
+  theShader.setUniform("u_time", millis() / 1000.0);
+  theShader.setUniform("u_mouse", [mouseX, map(mouseY, 0, height, height, 0)]);
+
+  // rect gives us some geometry on the screen
+  rect(0,0,width, height);
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
